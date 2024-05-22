1849ccaee74293a8ddfd073eaf5df84872935724
YehorKuzmenko <yehor.kuzmenko@etu.univ-amu.fr>
2024-05-22 14:29:55 +0200

    Do not move in menu

--- My project/Assets/Code/ATH et MENU/PauseMenuManager.cs
+++ My project/Assets/Code/ATH et MENU/PauseMenuManager.cs
@@ -4,11 +4,19 @@ using UnityEngine.SceneManagement;
 public class PauseMenuManager : MonoBehaviour
 {
     public GameObject pauseMenuUI;
+
+    private PlayerMovement playerMovement;
+
     void Start()
     {
         pauseMenuUI.SetActive(false);
-    }
+        playerMovement = FindObjectOfType<PlayerMovement>(); // Find the PlayerMovement script in the scene
 
+        if (playerMovement == null)
+        {
+            Debug.LogError("PlayerMovement script not found in the scene.");
+        }
+    }
 
     void Update()
     {
@@ -28,18 +36,30 @@ public class PauseMenuManager : MonoBehaviour
     public void PauseGame()
     {
         pauseMenuUI.SetActive(true);
+
+        if (playerMovement != null)
+        {
+            playerMovement.SetCanMove(false);
+        }
+
         Time.timeScale = 0f;
     }
 
     public void ResumeGame()
     {
         pauseMenuUI.SetActive(false);
+
+        if (playerMovement != null)
+        {
+            playerMovement.SetCanMove(true);
+        }
+
         Time.timeScale = 1f;
     }
 
     public void QuitToMainMenu()
     {
-        Time.timeScale = 1f; 
+        Time.timeScale = 1f;
         SceneManager.LoadScene("MenuPrincipal");
     }
 }
